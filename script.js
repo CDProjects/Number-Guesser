3174ae6b025152855ceb08297cdb56b61d042f44
Kiwicase <caseydentprojects@gmail.com>
2023-03-01 17:31:24 +0200

    compareGuesses function created

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ let currentRoundNumber = 1;
 function generateTarget() {
     return Math.floor(Math.random() * 10);
 }
+
+function compareGuesses(human, computer, generateTarget) {
+    if (Math.abs(human - generateTarget) < Math.abs(computer - generateTarget)) {
+        return true;
+    } else if (Math.abs(human - generateTarget) === Math.abs(computer - generateTarget)) {
+        return true;
+    } else {
+        return false;
+    }
+}
